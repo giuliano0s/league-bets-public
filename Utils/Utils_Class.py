bd2cf096f2cda56d417fc24eb8a6d49c0ee42b01
giuliano0s <giuliano.souza@grupoboticario.com.br>
2023-05-25 22:44:51 -0300

    added class utils

---
+++ Utils/Utils_Class.py
@@ -0,0 +1,42 @@
+##=================================IMPORTS=================================##
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, AdaBoostClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from xgboost import XGBRegressor, XGBClassifier
+
+import sys
+import os
+import json
+# sys.path.append(os.path.abspath
+#                 (os.path.join
+#                  (os.path.dirname("constants.py"), '..')))
+print('=================')
+ROOT_DIR = os.getcwd()
+sys.path.append(F'{ROOT_DIR}\Utils')
+from Utils.constants import *
+from scripts import *
+
+import warnings
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', None)
+warnings.filterwarnings('ignore')
+
+##=================================CLASS=================================##
+
+class Utils:
+
+    def __init__(self, matchList) -> None:
+        self.TARGET = 'Score'
+        self.CURRENT_YEAR = 2022
+        self.CURRENT_SEMESTER = 1
+        self.CURRENT_SEMESTER_YEAR = str(CURRENT_YEAR)+str(CURRENT_SEMESTER)
+        self.LAST_SEMESTER = abs(CURRENT_SEMESTER-1)
+        self.LAST_YEAR = CURRENT_YEAR-1 if LAST_SEMESTER==1 else CURRENT_YEAR
